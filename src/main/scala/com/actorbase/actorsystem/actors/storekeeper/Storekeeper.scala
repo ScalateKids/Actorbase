6c74837ce7df5f61b9882c9d2789b9d7679329e4
Alberto <albertodeagostini88@gmail.com>
2016-07-05 17:44:47 +0200

    debug

--- src/main/scala/com/actorbase/actorsystem/actors/storekeeper/Storekeeper.scala
+++ src/main/scala/com/actorbase/actorsystem/actors/storekeeper/Storekeeper.scala
@@ -109,7 +109,7 @@ class Storekeeper(private val collectionName: String, private val collectionOwne
         *
         */
       case GetItem(key)  =>
-        println("SK "+data.contains(key)+" of collection "+collectionName)
+        println("SK "+data.contains(key)+" of collection "+collectionName+"With data \n"+data)
         data get key map (v => sender ! Right(Response(CryptoUtils.bytesToAny(v)))) getOrElse sender ! Left("UndefinedKey")
 
       /**
